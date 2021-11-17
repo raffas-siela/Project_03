7496829529575d980b0e0c1da9a80e6145cdf842
raffas siela <raffas.siela@gmail.com>
2021-11-17 16:12:04 +0100

    changing in Roundcube class

--- src/com/selenium/Roundcube.java
+++ src/com/selenium/Roundcube.java
@@ -4,6 +4,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 public class Roundcube
     {
+        String login = "sm1017";
+        String password = "sm1017";
+        String phone = "060317090814";
+        String user = login + "@selenium.waw.pl";
+
         public void CreateNewUser()
         {
         //ścieżka do chromedriver
@@ -15,5 +20,13 @@ public class Roundcube
         driver.get("https://selenium.waw.pl/");
         //maksymalizacja okna przeglądarki
         driver.manage().window().maximize();
+        //pole "nazwa"
+        driver.findElement(By.id("rcmloginuser")).sendKeys(login);
+        //pole "hasło"
+        driver.findElement(By.id("password")).sendKeys(password);
+        //pole "powtórz hasło"
+        driver.findElement(By.id("confirm_password")).sendKeys(password);
+        // wypełnienie "birthday-day"
+        driver.findElement(By.name("birthday-day")).sendKeys("25");
         }
     }
