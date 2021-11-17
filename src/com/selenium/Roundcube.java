179ba2238c91aaed3380e8790548029e313da022
raffas siela <raffas.siela@gmail.com>
2021-11-17 16:00:11 +0100

    new class Roundcube

--- src/com/selenium/Roundcube.java
+++ src/com/selenium/Roundcube.java
@@ -1,9 +1,19 @@
 package com.selenium;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 public class Roundcube
     {
         public void CreateNewUser()
         {
-            System.out.println("początek");
+        //ścieżka do chromedriver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Rafał\\Desktop\\auto\\chromedriver\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+
+        System.out.println("Startujemy RoundCube");
+        //otwieramy stronę z adresem
+        driver.get("https://selenium.waw.pl/");
+        //maksymalizacja okna przeglądarki
+        driver.manage().window().maximize();
         }
     }
